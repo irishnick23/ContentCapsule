dd3a83cb86af7792a308f7cb3506283c32132fe1
Nick <na.white04@gmail.com>
2024-07-05 13:20:37 -0400

    Initial Commit

---
+++ ContentCapsule/ContentCapsuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentCapsuleApp.swift
+//  ContentCapsule
+//
+//  Created by Nicholas White on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ContentCapsuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
